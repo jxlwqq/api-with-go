310df60d841642311ba09d3e7efb02f5a5fe9788
jxlwqq <jxlwqq@gmail.com>
2019-08-09 17:05:04 +0800

    add mux router

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module api-with-go
 
 go 1.12
+
+require (
+	github.com/gorilla/mux v1.7.3
+	github.com/jinzhu/gorm v1.9.10
+)
--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+	"log"
+	"net/http"
+)
+
+type (
+	User struct {
+		gorm.Model
+		Name  string `json:"name"`
+		Email string `json:"email"`
+	}
+)
+
+func handleRequests() {
+	router := mux.NewRouter()
+	router.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprintf(writer, "Welcome!")
+	})
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
 
 func main() {
 	fmt.Println("Hello World!")
+
+	handleRequests()
 }
